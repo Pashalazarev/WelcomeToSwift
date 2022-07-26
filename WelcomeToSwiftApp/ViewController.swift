2e3654cb993a336688609e23ae7da3cf87083f59
Pashalazarev <mymessagebox9@gmail.com>
2022-07-26 13:08:14 +0300

    Initial Commit

---
+++ WelcomeToSwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WelcomeToSwiftApp
+//
+//  Created by Macbook on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
